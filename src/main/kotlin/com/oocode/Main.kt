6a26ffba4c4205196269fdbd5463fd99046e29d6
ape06 <ape06@ox.ac.uk>
2025-04-08 16:31:13 +0100

    first

--- src/main/kotlin/com/oocode/Main.kt
+++ src/main/kotlin/com/oocode/Main.kt
@@ -1,7 +1,18 @@
 @file:Suppress("UNUSED_PARAMETER")
 
+import com.oocode.Robot
+
+
 fun main(args: Array<String>) {
     println("Hello World!")
 }
 
-fun moo() = "boo"
+fun parse(command: String, robot: Robot): String {
+    val parts = command.split(" ")
+//    move 4 onto 7
+//    [move, 4, onto, 7]
+//    parts[1] = 4
+//    parts[3] = 7
+    robot.pileOver(parts[1].toInt(), parts[3].toInt())
+    return "Done"
+}
--- src/test/kotlin/com/oocode/MainKtTest.kt
+++ src/test/kotlin/com/oocode/MainKtTest.kt
@@ -1,13 +1,36 @@
 package com.oocode
 
-import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.equalTo
-import moo
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.eq
+import parse
 
 internal class MainKtTest {
     @Test
-    fun `moo is moo`() {
-        assertThat(moo(), equalTo("boo"))
+    fun test_1() {
+        val robotMock = mock(Robot::class.java)
+
+        parse("move 7 onto 4", robotMock)
+
+        verify(robotMock).moveOnto(7, 4)
+    }
+
+    @Test
+    fun test_2() {
+        val robotMock = mock(Robot::class.java)
+
+        parse("move 6 onto 4", robotMock)
+
+        verify(robotMock).moveOnto(6, 4)
+    }
+
+    @Test
+    fun test_3() {
+        val robotMock = mock(Robot::class.java)
+
+        parse("pile 8 over 6", robotMock)
+
+        verify(robotMock).pileOver(8, 6)
     }
 }
